c17d233265ce7bc073712dd32d45a4a2321fbb10
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-06-05 19:44:03 +0200

    refactor

--- script1.js
+++ script1.js
@@ -60,77 +60,65 @@ function updateNeuronSquare() {
     neuronSquare.style.background = `linear-gradient(to top, #6495ed ${filling}%, #ddd ${filling}% 100%)`;
 }
 
+function drawLine(context, startX, startY, endX, endY, color) {
+  context.beginPath();
+  context.moveTo(startX, startY);
+  context.lineTo(endX, endY);
+  context.strokeStyle = color;
+  context.lineWidth = 2;
+  context.stroke();
+}
+
 function drawLines() {
-    const neuronSquare = document.querySelector(".neuron-square");
-    const canvas = document.getElementById("lines-canvas");
-    const context = canvas.getContext("2d");
+  const neuronSquare = document.querySelector(".neuron-square");
+  const canvas = document.getElementById("lines-canvas");
+  const context = canvas.getContext("2d");
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-    context.clearRect(0, 0, canvas.width, canvas.height);
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
-    const neuronRect = neuronSquare.getBoundingClientRect();
-    const neuronX = neuronRect.left + neuronRect.width / 2;
-    const neuronY = neuronRect.top + neuronRect.height;
+  const neuronRect = neuronSquare.getBoundingClientRect();
+  const neuronX = neuronRect.left + neuronRect.width / 2;
+  const neuronY = neuronRect.top + neuronRect.height;
 
-    const square1 = document.querySelector(".square1");
-    const square2 = document.querySelector(".square2");
-    const square3 = document.querySelector(".square3");
-    const square4 = document.querySelector(".square4");
+  const square1 = document.querySelector(".square1");
+  const square2 = document.querySelector(".square2");
+  const square3 = document.querySelector(".square3");
+  const square4 = document.querySelector(".square4");
 
-    if (square1.classList.contains("on")) {
-        const square1Rect = square1.getBoundingClientRect();
-        const square1X = square1Rect.left + square1Rect.width / 2;
-        const square1Y = square1Rect.top;
+  if (square1.classList.contains("on")) {
+    const square1Rect = square1.getBoundingClientRect();
+    const square1X = square1Rect.left + square1Rect.width / 2;
+    const square1Y = square1Rect.top;
 
-        context.beginPath();
-        context.moveTo(neuronX, neuronY);
-        context.lineTo(square1X, square1Y);
-        context.strokeStyle = "#6bdf8e";
-        context.lineWidth = 2;
-        context.stroke();
-    }
+    drawLine(context, neuronX, neuronY, square1X, square1Y, "#6bdf8e");
+  }
 
-    if (square2.classList.contains("on")) {
-        const square2Rect = square2.getBoundingClientRect();
-        const square2X = square2Rect.left + square2Rect.width / 2;
-        const square2Y = square2Rect.top;
+  if (square2.classList.contains("on")) {
+    const square2Rect = square2.getBoundingClientRect();
+    const square2X = square2Rect.left + square2Rect.width / 2;
+    const square2Y = square2Rect.top;
 
-        context.beginPath();
-        context.moveTo(neuronX, neuronY);
-        context.lineTo(square2X, square2Y);
-        context.strokeStyle = "#6bdf8e";
-        context.lineWidth = 2;
-        context.stroke();
-    }
+    drawLine(context, neuronX, neuronY, square2X, square2Y, "#6bdf8e");
+  }
 
-    if (square3.classList.contains("on")) {
-        const square3Rect = square3.getBoundingClientRect();
-        const square3X = square3Rect.left + square3Rect.width / 2;
-        const square3Y = square3Rect.top;
+  if (square3.classList.contains("on")) {
+    const square3Rect = square3.getBoundingClientRect();
+    const square3X = square3Rect.left + square3Rect.width / 2;
+    const square3Y = square3Rect.top;
 
-        context.beginPath();
-        context.moveTo(neuronX, neuronY);
-        context.lineTo(square3X, square3Y);
-        context.strokeStyle = "#FF7F50";
-        context.lineWidth = 2;
-        context.stroke();
-    }
+    drawLine(context, neuronX, neuronY, square3X, square3Y, "#FF7F50");
+  }
 
-    if (square4.classList.contains("on")) {
-        const square4Rect = square4.getBoundingClientRect();
-        const square4X = square4Rect.left + square4Rect.width / 2;
-        const square4Y = square4Rect.top;
+  if (square4.classList.contains("on")) {
+    const square4Rect = square4.getBoundingClientRect();
+    const square4X = square4Rect.left + square4Rect.width / 2;
+    const square4Y = square4Rect.top;
 
-        context.beginPath();
-        context.moveTo(neuronX, neuronY);
-        context.lineTo(square4X, square4Y);
-        context.strokeStyle = "#6bdf8e";
-        context.lineWidth = 2;
-        context.stroke();
-    }
-
+    drawLine(context, neuronX, neuronY, square4X, square4Y, "#6bdf8e");
+  }
 }
 
 window.onload = function () {
